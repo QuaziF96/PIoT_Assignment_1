4894b0c80961f4a90ad771cd8050fb253693fb66
mashkovic <mashrurmahbubhyder95@gmail.com>
2020-04-05 19:35:25 +1000

    oop specific task b

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,6 +1,7 @@
 from sense_hat import SenseHat
 from time import sleep
 import json
+from Temperature import Temperature
 
 with open('config.json') as json_file:
     parsed_json = json.load(json_file)
@@ -11,25 +12,14 @@ comfortableMin = int(parsed_json['comfortable_min'])
 comfortableMax = int(parsed_json['comfortable_max'])
 hotMin = int(parsed_json['hot_min'])
 
-red = [255,0,0]
-green = [0,255,0]
-blue = [0,0,255]
-
 def senseTempAndDisplay():
     sense = SenseHat()
     while True:
         temp = int(round(float(sense.get_temperature())))
+
+        temperature = Temperature(temp, coldMax, hotMin, comfortableMax, comfortableMin, sense)
+        temperature.checkAndDisplay()
         
-        if(temp < coldMax):
-            #display number with blue
-            sense.show_message(str(temp),text_colour=blue)
-        elif (temp <= comfortableMax and temp >= comfortableMin):
-            #display number with green
-            sense.show_message(str(temp),text_colour=green)
-        elif (temp > hotMin):
-            print('hello')
-            #display number with red    
-            sense.show_message(str(temp),text_colour=red)
         sleep(10)    
 
 
