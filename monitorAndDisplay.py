7603098d668258f2f6e12f8f402c69a1b4f1ba81
mashkovic <mashrurmahbubhyder95@gmail.com>
2020-04-05 18:16:38 +1000

    Working task b

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -2,34 +2,35 @@ from sense_hat import SenseHat
 from time import sleep
 import json
 
-with open('config.jsson') as json_file:
-    parsed_json = json.loads(json_file)
+with open('config.json') as json_file:
+    parsed_json = json.load(json_file)
 
 
-coldMax = parsed_json['cold_max']
-comfortableMin = parsed_json['comfortable_min']
-comfortableMax = parsed_json['comfortable_max']
-hotMin = parsed_json['hot_min']
+coldMax = int(parsed_json['cold_max'])
+comfortableMin = int(parsed_json['comfortable_min'])
+comfortableMax = int(parsed_json['comfortable_max'])
+hotMin = int(parsed_json['hot_min'])
 
-red = (255,0,0)
-blue = (0,255,0)
-green = (0,0,255)
+red = [255,0,0]
+green = [0,255,0]
+blue = [0,0,255]
 
 def senseTempAndDisplay():
+    sense = SenseHat()
     while True:
-        sense =  SenseHat()
-        temp = round(sense.get_temperature)
-
+        temp = int(round(float(sense.get_temperature())))
+        
         if(temp < coldMax):
             #display number with blue
-            sense.show_letter(str(temp), text_colour = blue)
-        elif (temp <= 25 or temp >= 10):
+            sense.show_message(str(temp),text_colour=blue)
+        elif (temp <= comfortableMax and temp >= comfortableMin):
             #display number with green
-            sense.show_letter(str(temp), text_colour = green)
-        else:
+            sense.show_message(str(temp),text_colour=green)
+        elif (temp > hotMin):
+            print('hello')
             #display number with red    
-            sense.show_letter(str(temp), text_colour = red)
-        sleep(10)    
+            sense.show_message(str(temp),text_colour=red)
+        sleep(4)    
 
 
 senseTempAndDisplay()
